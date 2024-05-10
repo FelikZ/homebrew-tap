18616d58de378aca2f1d2117702fd6916cf1b573
Lukas Knuth <me@lknuth.dev>
2024-05-10 11:39:53 +0200

    FIX: Explicitly add `file:`-protocol to URL.

--- Casks/steelseries-exactmouse-tool.rb
+++ Casks/steelseries-exactmouse-tool.rb
@@ -2,7 +2,7 @@ cask "steelseries-exactmouse-tool" do
   version "1.0"
   sha256 "70f0b344d57b6d1405dd36b9bb5c14fb0d1fbb1397853a12c1b83a95652ba78b"
 
-  url "../bin/steelseries-exactmouse-tool-#{version}.dmg"
+  url "file://../bin/steelseries-exactmouse-tool-#{version}.dmg"
   name "SteelSeries ExactMouse Tool"
   desc "Truly removes mouse acceleration in Apple system"
   homepage "https://steelseries.com/downloads"
