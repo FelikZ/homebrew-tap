f2d8f406af2d679e9721187551e35953ff4dea3b
Lukas Knuth <me@lknuth.dev>
2024-05-10 08:57:13 +0200

    Added 'steelseries-exactmouse-tool' - which is deprecated on main Cask.
    See https://github.com/Homebrew/homebrew-cask/pull/173228

---
+++ Casks/steelseries-exactmouse-tool.rb
@@ -0,0 +1,20 @@
+cask "steelseries-exactmouse-tool" do
+  version "1.0"
+  sha256 "70f0b344d57b6d1405dd36b9bb5c14fb0d1fbb1397853a12c1b83a95652ba78b"
+
+  url "https://github.com/LukasKnuth/homebrew-tap.git/bin/steelseries-exactmouse-tool.dmg",
+      verified: "github.com/LukasKnuth/homebrew-tap",
+      branch: "main"
+  name "SteelSeries ExactMouse Tool"
+  desc "Truly removes mouse acceleration in macOS."
+  homepage "https://steelseries.com/downloads"
+
+  app "SteelSeries ExactMouse Tool.app"
+
+  uninstall quit: "com.SteelSeries.SteelSeries-ExactMouse-Tool"
+
+  zap trash: [
+    "~/Library/Caches/com.apple.helpd/Generated/com.SteelSeries.SteelSeries-ExactMouse-Tool.help",
+    "~/Library/Preferences/com.SteelSeries.SteelSeries-ExactMouse-Tool.plist",
+  ]
+end
